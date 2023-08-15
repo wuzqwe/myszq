c5d2b478c6cc2de24271d74f6fd13310fadb6fcb
szq <szq@qq.com>
2023-08-15 17:18:51 +0800

    我是hot-fit要合并到master

--- src/main/java/com/szq/myszq/hello/HelloWorld.java
+++ src/main/java/com/szq/myszq/hello/HelloWorld.java
@@ -3,5 +3,6 @@ package com.szq.myszq.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("我是hot-fit要合并到master");
     }
 }
