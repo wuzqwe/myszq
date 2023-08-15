7d6013be42022989bdf0a6a401e563778920a25e
szq <szq@qq.com>
2023-08-15 20:44:37 +0800

    push上去我是hot-fix分支

--- src/main/java/com/szq/myszq/hello/HelloWorld.java
+++ src/main/java/com/szq/myszq/hello/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("我是hot-fit要合并到master");
+        System.out.println("push上去我是hot-fix分支");
     }
 }
