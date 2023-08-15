ae611fcd5d93c5fd977d7679b905db8ff35510f3
wuzqwe <105253943+wuzqwe@users.noreply.github.com>
2023-08-15 20:54:53 +0800

    Update HelloWorld.java

--- src/main/java/com/szq/myszq/hello/HelloWorld.java
+++ src/main/java/com/szq/myszq/hello/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("我是hot-fit要合并到master");
         System.out.println("push上去我是hot-fix分支");
         System.out.println("hello啊");
+        System.out.println("hello啊szq");
     }
 }
